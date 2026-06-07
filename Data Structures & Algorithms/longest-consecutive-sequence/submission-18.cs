278c04be43d32298b11352f9e64a06020d237310
NJ-AIAgent <naincyjainyt@gmail.com>
2026-06-07 06:55:18 +0530

    Add: longest-consecutive-sequence - submission-18

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-18.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        if(nums.Length == 0) return 0;
+        Array.Sort(nums);
+        int count = 1;
+        int max = 1;
+        for(int i = 0;i<nums.Length-1;i++){
+            if(nums[i+1] == nums[i]){
+                continue;
+                }
+            else if(nums[i+1] == nums[i]+1){
+                count++;
+                max = Math.Max(max, count);
+            }
+            else {
+                count = 1;
+            }
+            
+        }
+    return max;
+    }
+}
