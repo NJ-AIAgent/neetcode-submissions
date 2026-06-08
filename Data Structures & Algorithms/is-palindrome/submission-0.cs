934279b7eefe78a3029aeb8d397d7e949f5c0a90
NJ-AIAgent <naincyjainyt@gmail.com>
2026-06-08 07:53:37 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        var str = new string(s.Where(char.IsLetterOrDigit).ToArray()).ToLower(); 
+
+        int i = 0; 
+        int j = str.Length-1;
+        while(j>i){
+            if(str[i] != str[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
